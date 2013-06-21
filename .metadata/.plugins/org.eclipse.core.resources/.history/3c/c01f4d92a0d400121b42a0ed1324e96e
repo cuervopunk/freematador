b15d2a5e64dbeaa3fd725d99e92cd4f831bfa7b6
Leonardo Sereno <leonardo.sereno@gmail.com>
2013-06-21 03:30:56 -0300

    Product Page - More Data

--- .metadata/.plugins/org.eclipse.core.resources/.history/3c/c01f4d92a0d400121b42a0ed1324e96e
+++
@@ -1,5 +0,0 @@
-package com.freematador.domain;
-
-public class Fee {
-
-}
--- elsubonlineWeb/src/com/freematador/presentation/HomeManagedBean.java
+++ elsubonlineWeb/src/com/freematador/presentation/HomeManagedBean.java
@@ -32,5 +32,7 @@ public class HomeManagedBean implements Serializable{
 		this.highlightedProducts = highlightedProducts;
 	}
 	
+	
+	
 
 }
